119f309b4be16beeb821db0785d90c8bb2dbb2fe
KodaHye <kodhdh09@gmail.com>
2023-03-28 20:26:03 +0900

    add: '1, 2, 3 더하기' 문제풀이 완료

--- BAEKJOON/BOJ9095.java
+++ BAEKJOON/BOJ9095.java
@@ -1,21 +1,26 @@
-
-
 import java.util.Scanner;
-
 /*
  * 1, 2, 3 더하기
- * n을 1, 2, 3의 합으로 나타내는 방법
  */
 public class BOJ9095 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
 		
-		for(int test_case = 1; test_case < T + 1; test_case++) {
-			int n = sc.nextInt();
-			
-			func(n);
+		int T = sc.nextInt();
+		int arr[] = new int[11];
+		
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 4;
+		
+		for(int i = 4; i < 11; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
 		}
+		for (int test_case = 1; test_case < T + 1; test_case++) {
+			int n = sc.nextInt();
+			sb.append(arr[n] + "\n");
+		}
+		System.out.println(sb);
 	}
 }
