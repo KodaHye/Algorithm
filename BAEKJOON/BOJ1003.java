a05995f43a158ca5c69307d1266ddcdabec73a0a
KodaHye <kodhdh09@gmail.com>
2023-03-31 00:04:54 +0900

    add: '피보나치 함수' 문제풀이 완료

--- BAEKJOON/BOJ1003.java
+++ BAEKJOON/BOJ1003.java
@@ -15,16 +15,8 @@ public class BOJ1003 {
 		
 		arr[0][0] = 1;
 		arr[0][1] = 0;
-		arr[1][0] = 0;
-		arr[1][1] = 1;
-		arr[2][0] = 1;
-		arr[2][1] = 1;
-		arr[3][0] = 2;
-		arr[3][1] = 1;
-		arr[4][0] = 2;
-		arr[4][1] = 3;
 		
-		for(int i = 5; i < 41; i++) {
+		for(int i = 1; i < 41; i++) {
 			arr[i][0] = arr[i - 1][1];
 			arr[i][1] = arr[i - 1][0] + arr[i - 1][1];
 		}
