a7ad8b463fcd893c1694e491d877689fe4efcadc
KodaHye <kodhdh09@gmail.com>
2023-06-02 02:23:56 +0900

    add: '트리의 지름' 문제풀이 완료

---
+++ BAEKJOON/BOJ1722.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+/*
+ * 순열의 순서
+ */
+public class BOJ1722 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	static int N;
+	static int k;
+	static int a, b, arr[];
+	static boolean sel[];
+	
+	public static void main(String[] args) throws Exception {
+		N  = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+	}
+}
