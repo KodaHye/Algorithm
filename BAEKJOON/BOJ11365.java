61553d8c9a36d735086441888aa75860f59a7d7e
KodaHye <kodhdh09@gmail.com>
2023-07-19 09:20:32 +0900

    add: '!밀비 급일' 문제풀이 완료

---
+++ BAEKJOON/BOJ11365.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BOJ11365 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            String input = sc.nextLine();
+
+            if(input.equals("END")) break;
+            for(int i = 0; i < input.length(); i++) sb.append(input.charAt(input.length() - 1 - i));
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
