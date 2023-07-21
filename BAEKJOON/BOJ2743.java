a45cc2a855a52b4a1adb5f693989c3d38ba531d8
KodaHye <kodhdh09@gmail.com>
2023-07-21 23:43:25 +0900

    add: '단어 길이 재기' 문제풀이 완료

---
+++ BAEKJOON/BOJ2743.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+단어 길이 재기
+ */
+public class BOJ2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(input.length());
+
+    }
+}
