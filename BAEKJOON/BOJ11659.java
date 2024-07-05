3816f92084618f04a845c9d3cd4448ceb9527c1d
KodaHye <kodhdh09@gmail.com>
2024-07-05 12:34:55 +0900

    feat: '구간 합 구하기 4' 문제풀이 완료

--- BAEKJOON/BOJ11659.java
+++ BAEKJOON/BOJ11659.java
@@ -1,38 +1,35 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
+/*
+구간 합 구하기 4
+ */
 
 public class BOJ11659 {
-
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("./BAEKJOON/Input/구간합구하기_4.txt"));
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
-		st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		
-		long[] S = new long[N + 1];
-		
-		// 배열 입력
+
+		int sum[] = new int[N + 1];
+
 		st = new StringTokenizer(br.readLine());
-		
-		for (int k = 1; k < N + 1; k++) {
-			S[k] = S[k - 1] + Integer.parseInt(st.nextToken());
+
+		for(int i = 1; i < N + 1; i++) {
+			sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
 		}
 
-		for(int k = 0; k < M; k++) {
+		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			sb.append((S[j] -S[i - 1]) + "\n");
-		}
-		System.out.println(sb);
-	}
 
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+
+			sb.append(sum[e] - sum[s - 1] + "\n");
+		}
+		System.out.print(sb);
+	}
 }
