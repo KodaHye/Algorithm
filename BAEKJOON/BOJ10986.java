cfb6d478b352eab429f3a128223bdabb8d11b0c6
KodaHye <kodhdh09@gmail.com>
2024-07-05 14:31:38 +0900

    feat: '나머지 합' 문제풀이 완료

--- BAEKJOON/BOJ10986.java
+++ BAEKJOON/BOJ10986.java
@@ -1,40 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/*
- * 나머지 합
- */
 public class BOJ10986 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N, M; 
-	static long S[], m[];
-	
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		m = new long[1001];
-		
-		S = new long[N + 1];
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 1; i < N + 1; i++) {
-			S[i] = (S[i - 1] + Integer.parseInt(st.nextToken())) % M;
-			m[(int) S[i]]++;
-		}
-		
-		long result = 0;
-		
-		for(int i = 0; i < M + 1; i++) {
-			if(m[i] != 0) result += (m[i] * (m[i] - 1)) / 2;
-		}
-		result += m[0];
-		
-		System.out.println(result);
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long sum[] = new long[N + 1];
+        long check[] = new long[M];
+
+        st = new StringTokenizer(br.readLine());
+        long result = 0;
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = (sum[i - 1] + Integer.parseInt(st.nextToken())) % M;
+            check[(int) sum[i]]++;
+        }
+
+        for(int i = 0; i < check.length; i++) {
+            if(check[i] != 0) result += check[i] * (check[i] - 1) / 2;
+        }
+        result += check[0];
+
+        System.out.println(result);
+    }
 }
