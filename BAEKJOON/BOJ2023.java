dbb72f8c19e2df1b30e0f5ba5d4fbd83c104436b
KodaHye <kodhdh09@gmail.com>
2024-07-09 21:12:21 +0900

    feat: '신기한 소수' 문제풀이 완료

--- BAEKJOON/BOJ2023.java
+++ BAEKJOON/BOJ2023.java
@@ -1,40 +1,38 @@
-import java.util.Scanner;
+import java.util.*;
 
 /*
- * 신기한 소수
+신기한 소수
  */
+
 public class BOJ2023 {
-	static int N;
-	static StringBuilder sb;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sb = new StringBuilder();
-		
-		N =sc.nextInt();
+    static StringBuilder sb = new StringBuilder();
 
-		func(0, N);
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
-	private static void func(int k, int n) {
-		if(n == 0) {
-			if(checkPrime(k)) sb.append(k).append("\n");
-			
-			return;
-		}
-		for(int i = 0; i < 10; i++) {
-			int tmp = k * 10 + i;
-			if(checkPrime(tmp)) func(tmp, n - 1);
-		}
-	}
+        dfs(0, N);
 
-	private static boolean checkPrime(int tmp) {
-		if(tmp < 2) return false;
-		for (int i = 2; i <= Math.sqrt(tmp); i++) {
-			if(tmp % i == 0) return false;
-		}
-		return true;
-	}
+        System.out.print(sb);
+    }
+
+    static void dfs(int k, int n) {
+        if(n == 0) {
+            if(isPrime(k)) sb.append(k + "\n");
+            return;
+        }
+
+        for(int i = 0; i < 10; i++) {
+            int tmp = k * 10 + i;
+            if(isPrime(tmp)) dfs(tmp, n - 1);
+        }
+    }
+    private static boolean isPrime(int num) {
+        if(num < 2) return false;
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if(num % i == 0) return false;
+        }
+
+        return true;
+    }
 }
