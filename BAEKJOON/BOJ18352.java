9dd492e0ccc894ed6c45b230fd052fe0c626e436
KodaHye <kodhdh09@gmail.com>
2024-07-15 14:47:03 +0900

    feat: '효율적인 해킹' 문제풀이 완료

--- BAEKJOON/BOJ18352.java
+++ BAEKJOON/BOJ18352.java
@@ -1,80 +1,56 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
- * 특정 거리의 도시 찾기
+특정 거리의 도시 찾기
  */
-public class BOJ18352 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N, M, K, X;
-	static ArrayList<Integer>[] adjList;
-	static int[] v;
-	static ArrayList<Integer> answer;
-	
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken()); // 도시의 개수
-		M = Integer.parseInt(st.nextToken()); // 도로의 개수
-		K = Integer.parseInt(st.nextToken()); // 거리 정보
-		X = Integer.parseInt(st.nextToken()); // 출발 도시의 번호
-		
-		adjList = new ArrayList[N + 1];
-		answer = new ArrayList<>();
-		
-		v = new int[N + 1];
-		Arrays.fill(v, -1);
-		
-		for (int i = 0; i < N + 1; i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			adjList[a].add(b);
-		}
-		
-		bfs(X);
-		
-		for (int i = 0; i < v.length; i++) {
-			if(v[i] == K) answer.add(i);
-		}
-		
-		if(answer.isEmpty()) System.out.println(-1);
-		else {
-			Collections.sort(answer);
-			
-			for (int ans : answer) {
-				System.out.println(ans);
-			}
-		}
-	}
 
-	private static void bfs(int X) {
-		Queue<Integer> queue = new LinkedList<>();
-		
-		queue.add(X);
-		v[X]++;
-		
-		while(!queue.isEmpty()) {
-			int now = queue.poll();
-			
-			for (Integer i: adjList[now]) {
-				if(v[i] == -1) {
-					v[i] = v[now] + 1;
-					queue.add(i);
-				}
-			}
-		}
-	}
+public class BOJ18352 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> adj[] = new ArrayList[N + 1];
+        boolean v[] = new  boolean[N + 1];
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<int[]> queue = new LinkedList<>();
+
+        for(int i = 0; i < adj.length; i++) adj[i] = new ArrayList<>();
+
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            adj[s].add(e);
+        }
+
+        queue.add(new int[] {X, 0});
+        v[X] = true;
+
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            int currentNum = current[0];
+            if(current[1] == K) list.add(currentNum);
+
+            for(int next: adj[currentNum]) {
+                if(v[next]) continue;
+                queue.add(new int[] {next, current[1] + 1});
+                v[next] = true;
+            }
+        }
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for(int n: list) sb.append(n + "\n");
+
+        System.out.print(list.size() == 0 ? -1 : sb);
+    }
 }
