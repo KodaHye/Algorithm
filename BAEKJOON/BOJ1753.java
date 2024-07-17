265ebff48f6bc64f114fd6b82320208369567d8d
KodaHye <kodhdh09@gmail.com>
2024-07-17 13:31:25 +0900

    feat: '최단경로' 문제풀이 완료

--- BAEKJOON/BOJ1753.java
+++ BAEKJOON/BOJ1753.java
@@ -1,80 +1,72 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
 최단경로
+pq 다익스트라로 구현할 때, 방문 처리하는 위치에 주의하기
  */
 
 public class BOJ1753 {
-    static class Node implements Comparable<Node> {
+    static class Node {
         int e, w;
 
         public Node(int e, int w) {
             this.e = e;
             this.w = w;
         }
-
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.w, o.w);
-        }
     }
-    static int V, E;
+
+    static boolean v[];
     static ArrayList<Node> adj[];
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws Exception {
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
         adj = new ArrayList[V + 1];
         for(int i = 0; i < adj.length; i++) adj[i] = new ArrayList<>();
-
-        int start = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
 
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            adj[s].add(new Node(e, w));
+            adj[u].add(new Node(v, w));
         }
 
-        int d[] = new int[V + 1];
+        int dis[] = new int[V + 1];
+        v = new boolean[V + 1];
 
-        Arrays.fill(d, Integer.MAX_VALUE);
-        boolean v[] = new boolean[V + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
 
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-
-        queue.add(new Node(start, 0));
-        d[start] = 0;
+        PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.w));
+        queue.add(new Node(K, 0));
+        dis[K] = 0;
 
         while(!queue.isEmpty()) {
             Node current = queue.poll();
 
-            for (Node next:adj[current.e]) {
-                if(d[next.e] > current.w + next.w) {
-                    d[next.e] = current.w + next.w;
-                    queue.add(new Node(next.e, d[next.e]));
+            if(v[current.e]) continue;
+            v[current.e] = true;
+
+            for(Node next: adj[current.e]) {
+
+                if(dis[next.e] > dis[current.e] + next.w) {
+                    dis[next.e] = dis[current.e] + next.w;
+                    queue.add(new Node(next.e, dis[next.e]));
                 }
             }
         }
 
-        for(int i = 1; i < d.length; i++) {
-            if(d[i] == Integer.MAX_VALUE) sb.append("INF");
-            else sb.append(d[i]);
-            sb.append("\n");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < dis.length; i++) {
+            sb.append((dis[i] == Integer.MAX_VALUE ? "INF" : dis[i]) + "\n");
         }
 
         System.out.print(sb);
