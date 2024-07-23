0f4071e2d5fb6637360e0307fb42d6bc606e5549
KodaHye <kodhdh09@gmail.com>
2024-07-23 11:48:12 +0900

    feat: '최소 스패닝 트리' 문제풀이 완료

--- BAEKJOON/BOJ1197.java
+++ BAEKJOON/BOJ1197.java
@@ -2,76 +2,77 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 최소 스패닝 트리
+최소 스패닝 트리
  */
+
 public class BOJ1197 {
-	static class Edge implements Comparable<Edge>{
-		int s, e, c;
+    static class Edge implements Comparable<Edge> {
+        int s, e, w;
 
-		public Edge(int s, int e, int c) {
-			super();
-			this.s = s;
-			this.e = e;
-			this.c = c;
-		}
+        public Edge(int s, int e, int w) {
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
 
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.c, o.c);
-		}
-	}
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int V, E, p[];
-	
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		p = new int[V + 1];
-		for (int i = 1; i < p.length; i++) {
-			p[i] = i;
-		}
-		
-		PriorityQueue<Edge> queue = new PriorityQueue<>();
-		
-		for(int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			
-			queue.add(new Edge(a, b, c));
-		}
-		
-		long result = 0;
-		
-		int cnt = 0;
-		while(!queue.isEmpty()) {
-			Edge current = queue.poll();
-			
-			if(find(current.s) == find(current.e)) continue;
-			union(current.s, current.e);
-			result += current.c;
-			
-			if(cnt == V - 1) break;
-		}
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
+    static int V, E, p[];
+    static PriorityQueue<Edge> pq;
 
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws Exception {
+        initInput();
+        mst();
+    }
 
-	private static void union(int a, int b) {
-		a = find(a);
-		b = find(b);
-		
-		if(a != b) {
-			p[b] = a;
-		}
-	}
+    public static void mst() {
+        int cnt = 0;
+        int result = 0;
 
-	private static int find(int a) {
-		return p[a] = p[a] == a ? a : find(p[a]);
-	}
+        while(cnt < V - 1) {
+            Edge e = pq.poll();
+
+            if(find(e.s) == find(e.e)) continue;
+            union(e.s, e.e);
+            result += e.w;
+            cnt++;
+        }
+
+        System.out.println(result);
+    }
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if(a != b) p[b] = a;
+    }
+    public static int find(int a) {
+        return a == p[a] ? p[a] : find(p[a]);
+    }
+    public static void initInput() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        p = new int[V + 1];
+        for(int i = 0; i < p.length; i++) p[i] = i;
+
+        pq = new PriorityQueue<>();
+
+        for(int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            pq.add(new Edge(a, b, c));
+        }
+    }
+
 }
