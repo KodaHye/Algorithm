24ea11fa6ae33b3d00fcf3be651f08568605369c
KodaHye <kodhdh09@gmail.com>
2024-07-24 10:45:21 +0900

    feat: '트리의 부모 찾기' 문제풀이 완료

--- BAEKJOON/BOJ11725.java
+++ BAEKJOON/BOJ11725.java
@@ -2,51 +2,56 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 트리의 부모 찾기
+트리의 부모 찾기
  */
+
 public class BOJ11725 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
-	static int N, p[];
-	static ArrayList<Integer> adj[];
-	static boolean v[];
-	
-	public static void main(String[] args) throws Exception {
-		N = Integer.parseInt(br.readLine());
+    static ArrayList<Integer> adj[];
+    static int p[];
 
-		adj = new ArrayList[N + 1];
-		p = new int[N + 1];
-		v = new boolean[N + 1];
-		
-		for(int i = 0; i < N + 1; i++) adj[i] = new ArrayList<>();
-		
-		for(int i = 0; i < N - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			adj[a].add(b);
-			adj[b].add(a);
-		}
-		
-		dfs(1);
-		
-		for(int i = 2; i < N + 1; i++) sb.append(p[i] + "\n");
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        initInput();
+        bfs();
+    }
 
-	private static void dfs(int k) {
-		v[k] = true;
-		
-		for (Integer next: adj[k]) {
-			if(!v[next]) {
-				v[next] = true;
-				p[next] = k;
-				dfs(next);
-			}
-		}
-	}
+    public static void bfs() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        p[1] = 1;
 
+        while(!queue.isEmpty()) {
+            int current = queue.poll();
+            for(int next: adj[current]) {
+                if(p[next] != 0) continue;
+                p[next] = current;
+                queue.add(next);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 2; i < p.length; i++) {
+            sb.append(p[i] + "\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static void initInput() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        adj = new ArrayList[N + 1];
+        p = new int[N + 1];
+
+        for(int i = 0; i < adj.length; i++) adj[i] = new ArrayList<>();
+
+        for(int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            adj[a].add(b);
+            adj[b].add(a);
+        }
+    }
 }
