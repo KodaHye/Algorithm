dc51c423d9a731fc7956ca8c8e5a8dc132e65fda
KodaHye <kodhdh09@gmail.com>
2024-07-24 11:46:27 +0900

    feat: '문자열 집합' 문제풀이 완료

--- BAEKJOON/BOJ14425.java
+++ BAEKJOON/BOJ14425.java
@@ -1,27 +1,54 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+/*
+문자열 집합
+ */
 
 public class BOJ14425 {
+    static class tNode {
+        tNode next[] = new tNode[26];
+        boolean isLeaf;
+    }
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int count = 0;
-		
-		HashSet<String> set1 = new HashSet<>();
-		
-		for(int i = 0; i < N; i++) {
-			set1.add(br.readLine());
-		}
-		for(int i = 0; i < M; i++) {
-			if(set1.contains(br.readLine())) count++;
-		}
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        tNode root = new tNode();
+
+        for(int k = 0; k < N; k++) {
+            String str = br.readLine();
+
+            tNode now = root;
+
+            for(int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if(now.next[c - 'a'] == null) now.next[c - 'a'] = new tNode();
+
+                now = now.next[c - 'a'];
+                if(i == str.length() - 1) now.isLeaf = true;
+            }
+        }
+
+        int cnt = 0;
+        for(int k = 0; k < M; k++) {
+            String str = br.readLine();
+
+            tNode now = root;
+            for(int i = 0; i < str.length(); i++) {
+                int c = str.charAt(i);
+
+                if(now.next[c - 'a'] == null) break;
+                now = now.next[c - 'a'];
+
+                if(i == str.length() - 1 && now.isLeaf) cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
 }
