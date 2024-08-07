90d008857a073ce5b8803ee9243df5497f635a7d
KodaHye <kodhdh09@gmail.com>
2024-08-07 09:26:20 +0900

    feat: '최대 힙' 문제풀이 완료

--- BAEKJOON/BOJ11279.java
+++ BAEKJOON/BOJ11279.java
@@ -1,34 +1,27 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /*
- * 최대 힙
+최대 힙
  */
+
 public class BOJ11279 {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				// TODO Auto-generated method stub
-				return -Integer.compare(o1, o2);
-			}
-		});
-		
-		int N = sc.nextInt();
-		
-		for(int i = 0; i < N; i++) {
-			int a = sc.nextInt();
-			if(a == 0) {
-				if(queue.isEmpty()) sb.append(0 + "\n");
-				else sb.append(queue.poll() + "\n");
-			} else {
-				queue.add(a);
-			}
-		}
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0) {
+                if(pq.isEmpty()) sb.append(0 + "\n");
+                if(!pq.isEmpty()) sb.append(pq.poll() + "\n");
+            } else pq.add(num);
+        }
+
+        System.out.print(sb);
+    }
 }
