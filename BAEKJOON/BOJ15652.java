55246dc5714f5f5bc47608cc972c97ab8208efb6
KodaHye <kodhdh09@gmail.com>
2024-08-08 10:18:42 +0900

    feat: 'N과 M (4)' 문제풀이 완료

--- BAEKJOON/BOJ15652.java
+++ BAEKJOON/BOJ15652.java
@@ -1,37 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+/*
+N과 M (4)
+ */
 
 public class BOJ15652 {
-	static int[] arr;
-	static boolean[] v;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		arr = new int[M];
-		v = new boolean[N];
-		func(0);
-		
-	}
-	
-	static int tmp = 0;
-	public static void func(int k) {
-		if(k == arr.length) {
-			for(int i = 0; i < arr.length; i++) {
-				System.out.print(arr[i] + " ");
-			} System.out.println();
-			return;
-		}
-		
-		for(int i = 0; i < v.length; i++) {
-			if(i >= tmp && !v[i]) {
-				arr[k] = i + 1;
-				tmp = i;
-				func(k + 1);
-				tmp = 0;
-			}
-		}
-	}
+    static int N, M, arr[];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        func(0, 0);
+        System.out.print(sb);
+    }
+
+    static void func(int idx, int num) {
+        if(idx == M) {
+            for(int i = 0; i < arr.length; i++) {
+                sb.append((arr[i] + 1) + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = num; i < N; i++) {
+            arr[idx] = i;
+            func(idx + 1, i);
+        }
+    }
 }
