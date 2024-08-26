4a5bf8c8cc95a9eb41d93f3569dba654e0f4f7cb
KodaHye <kodhdh09@gmail.com>
2024-08-27 00:20:17 +0900

    feat: '공주님을 구해라!' 문제풀이 완료

--- BAEKJOON/BOJ17836.java
+++ BAEKJOON/BOJ17836.java
@@ -1,104 +1,85 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
- * 공주님을 구해라
+공주님을 구해라!
  */
+
 public class BOJ17836 {
 	static class Point {
-		int r;
-		int c;
-		int t;
-		boolean hasGram;
-		public Point(int r, int c, int t, boolean hasGram) {
-			super();
+		int r, c, t;
+		boolean gram;
+		public Point(int r, int c, int t, boolean gram) {
 			this.r = r;
 			this.c = c;
 			this.t = t;
-			this.hasGram = hasGram;
+			this.gram = gram;
 		}
 	}
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
-	static int N, M, T, time, map[][];
+	static int N, M, T, result, map[][];
+	static int dr[] = {-1, 1, 0, 0}, dc[] = {0, 0, -1, 1};
 	static boolean v[][][];
-	static int dr[] = {1, -1, 0, 0};
-	static int dc[] = {0, 0, -1, 1};
-	static boolean flag;
-	
-	public static void main(String[] args) throws Exception {
-		st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		T = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		v = new boolean[2][N][M];
-		
-		for (int r = 0; r < map.length; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < map[r].length; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		bfs(0, 0);
-		
-		if(!flag) System.out.println("Fail");
-		else {
-			if(time > T) {
-				System.out.println("Fail");
-			} else {
-				System.out.println(time);
-			}
-		}
-	}
 
-	private static void bfs(int r, int c) {
-		Queue<Point> queue = new LinkedList<>();
-		
-		queue.offer(new Point(r, c, 0, false));
-		v[0][r][c] = true;
-		
-		while(!queue.isEmpty()) {
-			Point point = queue.poll();
-	
-			if(point.r == N - 1 && point.c == M - 1) {
-				flag = true;
-				time = point.t;
+	static boolean check(int r, int c) {
+		return r >= 0 && r < N && c >= 0 && c < M;
+	}
+	static void bfs() {
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(0, 0, 0, false));
+		v[0][0][0] = true;
+
+		while(!q.isEmpty()) {
+			Point current = q.poll();
+
+			if(current.r == N - 1 && current.c == M - 1) {
+				result = Math.min(current.t, result);
 				return;
 			}
-			
-			for(int i = 0; i < 4; i++) {
-				int nr = point.r + dr[i];
-				int nc = point.c + dc[i];
-				
+
+			for(int d = 0; d < 4; d++) {
+				int nr = current.r + dr[d];
+				int nc = current.c + dc[d];
+
 				if(!check(nr, nc)) continue;
-				if(!point.hasGram) { // 그램을 가지고 있지 않을 경우
-					if(!v[0][nr][nc] && map[nr][nc] == 0) {
-						queue.add(new Point(nr, nc, point.t + 1, point.hasGram));
-						v[0][nr][nc] = true;
-					} else if(!v[0][nr][nc] && map[nr][nc] == 2) {
-						queue.add(new Point(nr, nc, point.t + 1, true));
+
+				if(current.gram) {
+					if(v[1][nr][nc]) continue;
+					q.add(new Point(nr, nc, current.t + 1, true));
+					v[1][nr][nc] = true;
+				} else {
+					if(v[0][nr][nc]) continue;
+					if(map[nr][nc] == 0) {
+						q.add(new Point(nr, nc, current.t + 1, false));
 						v[0][nr][nc] = true;
 					}
-				}
-				else { // 그램을 가지고 있을 경우
-					if(!v[1][nr][nc]) {
-						queue.add(new Point(nr, nc, point.t + 1, point.hasGram));
+					if(map[nr][nc] == 2) {
+						q.add(new Point(nr, nc, current.t + 1, true));
 						v[1][nr][nc] = true;
 					}
 				}
 			}
 		}
 	}
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	private static boolean check(int nr, int nc) {
-		return nr >= 0 && nr < N && nc >= 0 && nc < M;
+		String input[] = br.readLine().split(" ");
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
+		T = Integer.parseInt(input[2]);
+
+		result = Integer.MAX_VALUE;
+		map = new int[N][M];
+		v = new boolean[2][N][M];
+
+		for(int r = 0; r < N; r++) {
+			String s = br.readLine();
+			for(int c = 0; c < M; c++) {
+				map[r][c] = s.charAt(c * 2) - '0';
+			}
+		}
+
+		bfs();
+		System.out.println(result == Integer.MAX_VALUE || result > T ? "Fail" : result);
 	}
 }
