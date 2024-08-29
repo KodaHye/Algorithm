0d66f31605717e993de14577273ae014c8415923
KodaHye <kodhdh09@gmail.com>
2024-08-29 12:07:38 +0900

    feat: '소수 구하기' 문제풀이 완료

--- BAEKJOON/BOJ1929.java
+++ BAEKJOON/BOJ1929.java
@@ -1,38 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
 소수 구하기
  */
+
 public class BOJ1929 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static int M, N;
+    static boolean isNotPrime[];
 
+    static void getPrime() {
+        isNotPrime[0] = isNotPrime[1] = true;
+
+        for(int i = 2; i < Math.sqrt(isNotPrime.length); i++) {
+            if(isNotPrime[i]) continue;
+            for(int j = 2 * i; j < isNotPrime.length; j+= i) isNotPrime[j] = true;
+        }
+    }
     public static void main(String[] args) throws Exception {
-        st = new StringTokenizer(br.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        boolean prime[] = new boolean[N + 1];
 
-        /*
-        에라토스테네스의 체
-        소수의 배수를 지우면 소수가 된다.
-         */
-
-        prime[0] = prime[1] = true;
-
-        for(int i = 2; i < N + 1; i++) {
-            if(!prime[i]) {
-                for(int j = 2; i * j < N + 1; j++) prime[i * j] = true;
-            }
-        }
+        isNotPrime = new boolean[N + 1];
+        getPrime();
 
         for(int i = M; i < N + 1; i++) {
-            if(!prime[i]) sb.append(i).append("\n");
+            if(isNotPrime[i]) continue;
+            sb.append(i + "\n");
         }
 
         System.out.print(sb);
