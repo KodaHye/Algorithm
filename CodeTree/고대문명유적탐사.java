9b7a3000537eb67707756cba1e5cec456ca25db4
KodaHye <kodhdh09@gmail.com>
2024-10-09 14:44:29 +0900

    feat: '고대문명 유적탐사' 문제풀이 완료

--- CodeTree/고대문명유적탐사.java
+++ CodeTree/고대문명유적탐사.java
@@ -2,212 +2,209 @@ import java.io.*;
 import java.util.*;
 
 /*
- * 고대 문명 유적 탐사   		
+ * 배열 참조 & 복사 정신 차리고 똑바로 하기!
+ * 우선 순위 잘 설정하기
+ * 단계별로 구현 잘 하기!!
  */
 
 public class 고대문명유적탐사 {
-	static class Point implements Comparable<Point> {
+	static class Point {
 		int r, c;
-
 		public Point(int r, int c) {
 			this.r = r;
 			this.c = c;
 		}
-
-		@Override
-		public int compareTo(Point o) {
-			if (this.c == o.c)
-				return -1 * Integer.compare(this.r, o.r);
-			return Integer.compare(this.c, o.c);
-		}
 	}
-
-	static class RotateInfo {
-		Point p;
-		int n;
-
-		public RotateInfo(Point p, int n) {
-			this.p = p;
-			this.n = n;
-		}
-	}
-
+	static int[][] map;
+	static int[] arr;
+	static int K, M, idx;
+	static int[] dr = {-1, 1, 0, 0}, dc = {0, 0, -1, 1};
 	static StringBuilder sb = new StringBuilder();
-	static int dr[] = { -1, 1, 0, 0 };
-	static int dc[] = { 0, 0, -1, 1 };
-	static RotateInfo rotateInfo;
-	static Queue<Point> queue;
-	static PriorityQueue<Point> tmpQueue, pq;
-	static int K, M, map[][], tmp[][], idx;
-	static boolean v[][];
-	static int arr[];
-
+	
+	static void solution() {
+		while(K-- > 0) {
+			int[][] copy = new int[5][5];
+			int result = 0;
+			int maxScore = 0;
+	
+			boolean rotateFlag = false;
+			
+			for(int d = 1; d < 4; d++) {
+				for(int c = 1; c < 4; c++) {
+					for(int r = 1; r < 4; r++) {
+						int rotateMap[][] = rotateMap(r, c, d);
+						
+						int score = getScore(rotateMap);
+						if(maxScore < score) {
+							
+							maxScore = score;
+							copy = rotateMap;
+							rotateFlag = true;
+						}
+					}
+				}
+			}
+			
+			if(!rotateFlag) break;
+			map = copy;
+			
+			result = getScore();
+			for(int r = 0; r < map.length; r++) {
+				System.out.println(Arrays.toString(map[r]));
+			}
+			
+			while(true) {
+				fillMap();
+//				System.out.println("fillMap 하고 나서");
+				int newScore = getScore();
+				
+//				System.out.println("getScore 이후");
+				if(newScore == 0) break;
+				result += newScore;
+				
+				System.out.println();
+			}
+			
+			sb.append(result + " "); 
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static int getScore() {
+		int result = 0;
+		
+		boolean[][] v = new boolean[5][5];
+		
+		for(int r = 0; r < 5; r++) {
+			for(int c = 0; c < 5; c++) {
+				
+				if(v[r][c]) continue;
+				Deque<Point> q = new ArrayDeque<Point>();
+				Deque<Point> tmp = new ArrayDeque<Point>();
+				
+				int cnt = 1;
+				q.add(new Point(r, c));
+				tmp.add(new Point(r, c));
+				
+				v[r][c] = true;
+				
+				while(!q.isEmpty()) {
+					Point current = q.poll();
+		
+					for(int d = 0; d < 4; d++) {
+						int nr = current.r + dr[d];
+						int nc = current.c + dc[d];
+						
+						if(!check(nr, nc) || v[nr][nc] || map[nr][nc] != map[r][c]) continue;
+						cnt++;
+						q.add(new Point(nr, nc));
+						tmp.add(new Point(nr, nc));
+						
+						v[nr][nc] = true;
+					}
+				}
+				
+				if(cnt >= 3) {
+					while(!tmp.isEmpty()) {
+						Point current = tmp.poll();
+						map[current.r][current.c] = 0;
+					}
+					
+					result += cnt;
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	static void fillMap() {
+		for(int c = 0; c < 5; c++) {
+			for(int r = 4; r >= 0; r--) {
+				if(map[r][c] != 0) continue;
+				map[r][c] = arr[idx];
+				idx = (idx + 1) % arr.length;
+			}
+		}
+	}
+	
+	static int getScore(int[][] rotateMap) {
+		int score = 0;
+		boolean[][] v = new boolean[5][5];
+		Deque<Point> q = new ArrayDeque<Point>();
+		
+		for(int r = 0; r < 5; r++) {
+			for(int c = 0; c < 5; c++) {
+				if(v[r][c]) continue;
+				
+				int tmp = 1;
+				
+				q.add(new Point(r, c));
+				v[r][c] = true;
+				
+				while(!q.isEmpty()) {
+					Point current = q.poll();
+					for(int d = 0; d < 4; d++) {
+						int nr = current.r + dr[d];
+						int nc = current.c + dc[d];
+						
+						if(!check(nr, nc) || v[nr][nc] || rotateMap[nr][nc] != rotateMap[r][c]) continue;
+						v[nr][nc] = true;
+						q.add(new Point(nr, nc));
+						tmp++;
+					}
+				}
+				
+				if(tmp > 2) score += tmp;
+			}
+		}
+		return score;
+	}
+	
+	static boolean check(int r, int c) {
+		return r >= 0 && r < 5 && c >= 0 && c < 5;
+	}
+	
+	static int[][] rotateMap(int r, int c, int d) {
+		int[][] rotateMap = new int[5][5];
+		for(int i = 0; i < 5; i++) rotateMap[i] = Arrays.copyOf(map[i], 5);
+		
+		for(int dr = -1; dr < 2; dr++) {
+			for(int dc = -1; dc < 2; dc++) {
+				if(d == 1) rotateMap[r + dc][c - dr] = map[r + dr][c + dc];
+				if(d == 2) rotateMap[r - dr][c - dc] = map[r + dr][c + dc];
+				if(d == 3) rotateMap[r - dc][c + dr] = map[r + dr][c + dc];
+			}
+		}
+		
+		return rotateMap;
+	}
 	public static void main(String[] args) throws Exception {
 		initInput();
 		solution();
 	}
-
-	private static void solution() {
-		while (K-- > 0) {
-
-			int cnt = 0;
-
-			int findRotateMax = findRoateMax();
-			if (findRotateMax == 0) break;
-
-			rotate(rotateInfo.p.r, rotateInfo.p.c, rotateInfo.n);
-
-			for (int r = 0; r < map.length; r++) map[r] = Arrays.copyOf(tmp[r], tmp[r].length);
-
-			while (true) {
-				pq.clear();
-				int getMaxValue = getMaxValue();
-				if (getMaxValue == 0) break;
-
-				cnt += getMaxValue;
-				while (!pq.isEmpty()) {
-					Point current = pq.poll();
-
-					tmp[current.r][current.c] = arr[idx];
-					idx = (idx + 1) % arr.length;
-				}
-			}
-
-			for (int r = 0; r < map.length; r++)
-				map[r] = Arrays.copyOf(tmp[r], tmp[r].length);
-			sb.append(cnt + " ");
-		}
-
-		System.out.println(sb);
-	}
-
-	private static int findRoateMax() {
-		int cnt = 0;
-
-		for (int k = 1; k < 1 + 3; k++) {
-			for (int c = 1; c < 4; c++) {
-				for (int r = 1; r < 4; r++) {
-					int tmp = rotate(r, c, k);
-					if (cnt < tmp) {
-						cnt = tmp;
-						modifyRotateInfo(r, c, k);
-					}
-				}
-			}
-		}
-
-		return cnt;
-	}
-
-	private static int rotate(int i, int j, int n) {
-		for (int r = 0; r < tmp.length; r++)
-			tmp[r] = Arrays.copyOf(map[r], map[r].length);
-
-		for (int r = i - 1; r < i + 2; r++) {
-			for (int c = j - 1; c < j + 2; c++) {
-				if (r == i && c == j)
-					continue;
-
-				int a = r - i;
-				int b = c - j;
-				if (n == 1) tmp[i + b][j - a] = map[r][c];
-				else if (n == 2) tmp[i - a][j - b] = map[r][c];
-				else if (n == 3) tmp[i - b][j + a] = map[r][c];
-			}
-		}
-
-		return getMaxValue();
-	}
-
-	private static int getMaxValue() {
-		for (int r = 0; r < v.length; r++) {
-			Arrays.fill(v[r], false);
-		}
-
-		int total = 0;
-
-		for (int r = 0; r < v.length; r++) {
-			for (int c = 0; c < v[0].length; c++) {
-				if (v[r][c]) continue;
-				int tmp = bfs(r, c);
-				if (tmp > 2) {
-					total += tmp;
-					pq.addAll(tmpQueue);
-				}
-			}
-		}
-
-		return total;
-	}
-
-	private static int bfs(int r, int c) {
-		int cnt = 1;
-
-		queue.clear();
-		tmpQueue.clear();
-
-		queue.add(new Point(r, c));
-		tmpQueue.add(new Point(r, c));
-
-		v[r][c] = true;
-		while (!queue.isEmpty()) {
-			Point current = queue.poll();
-
-			for (int d = 0; d < 4; d++) {
-				int nr = current.r + dr[d];
-				int nc = current.c + dc[d];
-
-				if (!check(nr, nc) || v[nr][nc] || (tmp[nr][nc] != tmp[current.r][current.c]))
-					continue;
-
-				v[nr][nc] = true;
-				queue.add(new Point(nr, nc));
-				tmpQueue.add(new Point(nr, nc));
-				cnt++;
-			}
-		}
-
-		return cnt;
-	}
-
-	private static boolean check(int nr, int nc) {
-		return nr >= 0 && nr < 5 && nc >= 0 && nc < 5;
-	}
-
-	private static void modifyRotateInfo(int r, int c, int n) {
-		rotateInfo.p.r = r;
-		rotateInfo.p.c = c;
-		rotateInfo.n = n;
-	}
-
-	private static void initInput() throws Exception {
-
-		System.setIn(new FileInputStream("./input/고대문명유적탐사.txt"));
+	
+	static void initInput() throws Exception {
+//		System.setIn(new FileInputStream("./input/고대문명유적탐사.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		rotateInfo = new RotateInfo(new Point(0, 0), 0);
-		queue = new LinkedList<Point>();
-		tmpQueue = new PriorityQueue<Point>();
-		pq = new PriorityQueue<Point>();
+			
 		map = new int[5][5];
-		tmp = new int[5][5];
-		v = new boolean[5][5];
+
 		K = Integer.parseInt(st.nextToken()); // 탐사 반복 횟수
 		M = Integer.parseInt(st.nextToken()); // 벽면에 적힌 유물 조각의 개수
-
-		String s = null;
-
-		for (int r = 0; r < map.length; r++) {
-			s = br.readLine();
-			for (int c = 0; c < map[0].length; c++) {
-				map[r][c] = s.charAt(c * 2) - '0';
+		
+		arr = new int[M];
+		
+		for(int r = 0; r < 5; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c = 0; c < 5; c++) {
+				map[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		arr = new int[M];
-		s = br.readLine();
-
-		for (int i = 0; i < M; i++) arr[i] = s.charAt(i * 2) - '0';
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(st.nextToken());
 	}
 }
